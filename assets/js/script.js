fae6e6f5a3612fce2ea35741d7e4eef49347a82f
Osvaldo Domingos <osvaldod.mariquele@gmail.com>
2018-03-14 15:05:40 -0700

    Remove some bad code

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,16 +15,11 @@ $(function(){
 		step(2);
 	});
 
-
-
-
-
 	$('#step2 .button').click(function(){
 		$(this).parent().find('input').click();
 	});
 
 	var file = null;
-
 	$('#step2').on('change', '#encrypt-input', function(e){
 		if(e.target.files.length!=1){
 			alert('Selecione o ficheiro que deseja encriptar!');
@@ -82,31 +77,14 @@ $(function(){
 			// Encrypt the file!
 
 			reader.onload = function(e){
-
-				// Use the CryptoJS library and the AES cypher to encrypt the 
-				// contents of the file, held in e.target.result, with the password
-
 				var encrypted = CryptoJS.AES.encrypt(e.target.result, password);
-
-				// The download attribute will cause the contents of the href
-				// attribute to be downloaded when clicked. The download attribute
-				// also holds the name of the file that is offered for download.
-
 				a.attr('href', 'data:application/octet-stream,' + encrypted);
 				a.attr('download', file.name + '.encrypted');
 
 				step(4);
 			};
-
-			// This will encode the contents of the file into a data-uri.
-			// It will trigger the onload handler above, with the result
-
 			reader.readAsDataURL(file);
-		}
-		else {
-
-			// Decrypt it!
-
+		} else {
 			reader.onload = function(e){
 
 				var decrypted = CryptoJS.AES.decrypt(e.target.result, password)
@@ -127,25 +105,13 @@ $(function(){
 		}
 	});
 
-
-	/* The back button */
-
-
 	back.click(function(){
-
-		// Reinitialize the hidden file inputs,
-		// so that they don't hold the selection 
-		// from last time
-
 		$('#step2 input[type=file]').replaceWith(function(){
 			return $(this).clone();
 		});
 
 		step(1);
 	});
-
-
-	// Helper function that moves the viewport to the correct step div
 
 	function step(i){
 
@@ -155,11 +121,6 @@ $(function(){
 		else{
 			back.fadeIn();
 		}
-
-		// Move the #stage div. Changing the top property will trigger
-		// a css transition on the element. i-1 because we want the
-		// steps to start from 1:
-
 		stage.css('top',(-(i-1)*100)+'%');
 	}
 
